48c55825a6e59849d617f374231efaffc1baa8f3
JurajKubrican <juraj.kubrican@erstegroup.com>
2025-07-20 22:49:31 +0200

    location

--- app/(tabs)/meteogram.tsx
+++ app/(tabs)/meteogram.tsx
@@ -1,10 +1,82 @@
 import Meteogram from '@/components/image'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Button, Text } from 'react-native'
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 
-export default function Tab() {
+const getDistance = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+) => {
+  const difLat = Math.abs(lat1 - lat2)
+  const difLon = Math.abs(lon1 - lon2)
+  return Math.sqrt(difLat * difLat + difLon * difLon)
+}
+
+type ShmuLocation = {
+  station_id: number
+  station_name: string
+  lat: string
+  lon: string
+  district_code?: number
+}
+type ShmuLocationWithDistance = ShmuLocation & { distance: number }
+
+const getLocations = async () => {
+  const response = await fetch(
+    'https://www.shmu.sk/api/v1/nwp/getjsonaladinstations',
+  )
+  return response.json()
+}
+
+export default () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
+
+  const { data } = useQuery<ShmuLocation[]>({
+    queryKey: ['locations'],
+    queryFn: getLocations,
+  })
+
+  const closestLocation = data?.reduce<ShmuLocationWithDistance>((closest, current) => {
+    if (!location) return closest
+
+    const currentDistance = getDistance(
+      parseFloat(current.lat),
+      parseFloat(current.lon),
+      location.coords.latitude,
+      location.coords.longitude,
+    )
+    if (currentDistance < closest.distance) {
+      return {...current, distance: currentDistance }
+    }
+    return closest
+    
+  }, {...data[0], distance: Infinity}) || null
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    }
+
+    getCurrentLocation()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Meteogram />
+      <Text>{location?.coords.latitude} - {location?.coords.longitude}</Text>
+      <Text>{closestLocation?.station_name}</Text>
+      <Text>Distance: {closestLocation?.distance.toFixed(2)} km</Text>
+
     </View>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { registerRootComponent } from 'expo'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   )
 }
